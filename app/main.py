fce425485dee9da498abcb336e8f944a93635abf
Sumit Prakash <prakash@Sumits-MackBook-Pro.local>
2025-03-21 17:16:59 +0530

    added CORS issue

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import users, notification, project, feedback, sent_email
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",  
+    "http://127.0.0.1:5500",
+    "*",  
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 app.include_router(users.router, prefix="/api/users", tags=["Users"])
 
 @app.get("/")
